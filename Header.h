db7900cb16510365c8dc54fcae142da2d5b433f5
kerrygrover <prateekgrover.rns@gmail.com>
2015-09-18 17:44:39 +0530

    Some minor change

--- Header.h
+++ Header.h
@@ -2,8 +2,8 @@
 //  Header.h
 //  iFlyChatExampleSwiftChatView
 //
-//  Created by Prateek Grover on 05/08/15.
-//  Copyright (c) 2015 Prateek Grover. All rights reserved.
+//  Created by iFlyLabs on 05/08/15.
+//  Copyright (c) 2015 iFlyLabs. All rights reserved.
 //
 
 #ifndef iFlyChatExampleSwiftChatView_Header_h
--- iFlyChatExampleSwiftChatView/ViewController.swift
+++ iFlyChatExampleSwiftChatView/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  iFlyChatExampleSwiftChatView
 //
-//  Created by Prateek Grover on 09/09/15.
-//  Copyright (c) 2015 Prateek Grover. All rights reserved.
+//  Created by iFlyLabs on 09/09/15.
+//  Copyright (c) 2015 iFlyLabs. All rights reserved.
 //
 
 import UIKit
